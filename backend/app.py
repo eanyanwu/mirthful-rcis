bc4fb7aafd5187e03b1d49894bf59bc3abcfc583
eanyanwu <ezeanyinabia.anyanwu@gmail.com>
2019-04-05 00:09:49 -0400

    Proof of concept for user_id based authentication
    A user id will be enough to be authenticated with the system. This means
    that if an "attacker" gets a hold of a user's id, they can impersonate
    him forever.
    I have determined that I am ok with this given the type of application
    I'm building.
    The user_id will be long enough that brute-forcing won't be useful.
    QR codes will be used to "automagically" log the user in.

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,33 @@
+import sqlite3
 from flask import Flask
+from flask import request
+from flask import session
+from flask import json
+from flask import abort
 
 app = Flask(__name__)
+app.secret_key = b'qwerty' # TODO: obviously change this
 
-@app.route('/')
-def hello_world():
-    return 'Hello, world'
+@app.route('/session/<string:user_id>', methods=['GET'])
+def start_session(user_id):
+    # Connect to the database
+    connection = sqlite3.connect('./rci.db')
+    
+    # Use sqlite3.Row as a row_factory so that we can access our rows by column name
+    connection.row_factory = sqlite3.Row
+
+    # What even is a cursor
+    cursor = connection.cursor()
+
+    # Find the user in question
+    user_record = cursor.execute("""
+    SELECT * 
+    FROM users
+    WHERE user_id = ?""", (user_id,)).fetchone()
+
+    if not user_record:
+        abort(401)
+
+    session['user_id'] = user_record['user_id']
+
+    return json.jsonify({})
