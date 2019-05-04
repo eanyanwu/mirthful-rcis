5d8a3913b0d53d725fa84e88ae55c1bed11d3231
eanyanwu <ezeanyinabia.anyanwu@gmail.com>
2019-05-04 01:35:03 -0400

    Pass username and password from form to api

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,24 @@
-console.log(authentication);
+var loginForm = document.querySelector('form#login-form');
+
+// Register event listners
+loginForm.addEventListener('submit', onLoginSubmit);
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    var loginForm = event.currentTarget;
+    var username = loginForm.querySelector("input[name='username']").value;
+    var password = loginForm.querySelector("input[name='password']").value;
+
+    console.log(username, password);
+
+    authentication.login(username, password,
+        function(result) {
+            console.log('Success', result);
+        },
+        function(status, error) {
+            console.log(status, error);
+        });
+}
 
 
-authentication.login('test_student', 'test_student',
-    function(result) {
-        console.log('Success', result);
-    },
-    function(status, error) {
-        console.log(status, error);
-    });
+
