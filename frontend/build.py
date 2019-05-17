e3681703069cdb30f790fc072222c13694af2116
eanyanwu <ezeanyinabia.anyanwu@gmail.com>
2019-05-17 00:18:55 -0400

    Gave up and implemented small a filesystem watcher
    Third-party solutions that would work on both mac/pc and linux were too
    hard to find or too complex. The only one I foud that worked was
    fswatch. Setting it up took too long and I am still unsure how it would
    work on windows.

--- frontend/build.py
+++ frontend/build.py
@@ -20,54 +20,60 @@ def filter_func(template_name):
 
     return is_html and is_child_template 
 
-# The location from which templates will be discovered
-current_dir = os.path.dirname(os.path.abspath(__file__))
+def apply_templates():
+    # The location from which templates will be discovered
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# This is where we will output the files to be served
-output_dir = os.path.join(current_dir, "www")
+    # This is where we will output the files to be served
+    output_dir = os.path.join(current_dir, "www")
 
-# We will copy the css and js folders as-is
-css_dir = os.path.join(current_dir, 'css')
-js_dir = os.path.join(current_dir, 'js')
+    # We will copy the css and js folders as-is
+    css_dir = os.path.join(current_dir, 'css')
+    js_dir = os.path.join(current_dir, 'js')
 
-# Clear the output_dir if it already exists
-if os.path.exists(output_dir):
-    shutil.rmtree(output_dir)
+    # Clear the output_dir if it already exists
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
 
-os.mkdir(output_dir)
+    os.mkdir(output_dir)
 
-# Copy css files
-shutil.copytree(css_dir, os.path.join(output_dir, 'css'))
+    # Copy css files
+    shutil.copytree(css_dir, os.path.join(output_dir, 'css'))
 
-# Copy js files
-shutil.copytree(js_dir, os.path.join(output_dir, 'js'))
+    # Copy js files
+    shutil.copytree(js_dir, os.path.join(output_dir, 'js'))
 
 
-# Create the jinja2 environment 
-# All templates are loaded into this, and it looks like that is what
-# allows template inheritance
-env = Environment (
-    loader=FileSystemLoader(current_dir)
-)
+    # Create the jinja2 environment 
+    # All templates are loaded into this, and it looks like that is what
+    # allows template inheritance
+    env = Environment (
+        loader=FileSystemLoader(current_dir)
+    )
 
-# We only want to render child templates
-child_templates = env.list_templates(filter_func=filter_func)
+    # We only want to render child templates
+    # Meaning, we only want to render *.html files that inherit from something
+    # else
+    child_templates = env.list_templates(filter_func=filter_func)
 
-for template_name in child_templates:
-    template = env.get_template(template_name)
-    rendered = template.render()
+    for template_name in child_templates:
+        template = env.get_template(template_name)
+        rendered = template.render()
 
-    # Create the full path for the output file
-    filename = template.filename
-    leaf_part = filename[filename.find('html'):].replace('html/', '')
-    new_path = os.path.join(output_dir, leaf_part)
+        # Create the full path for the output file
+        filename = template.filename
+        leaf_part = filename[filename.find('html'):].replace('html/', '')
+        new_path = os.path.join(output_dir, leaf_part)
 
-    # Create any intermediate directories
-    new_path_dir = os.path.dirname(new_path)
-    os.makedirs(new_path_dir, exist_ok=True)
+        # Create any intermediate directories
+        new_path_dir = os.path.dirname(new_path)
+        os.makedirs(new_path_dir, exist_ok=True)
 
-    # Write the file
-    with open(new_path, mode='w') as f:
-        f.write(rendered)
+        # Write the file
+        with open(new_path, mode='w') as f:
+            f.write(rendered)
 
+
+if __name__ == '__main__':
+    apply_templates()
 
